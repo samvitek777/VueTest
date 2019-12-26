173ec47764236a3b0368c38b70da4b7dfe24c076
Victor Samoylenko <samvitek1@gmail.com>
2019-12-26 16:28:09 +0300

    first commit

---
+++ src/main/java/com/example/VueSpringTest/VueSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.VueSpringTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VueSpringTest/VueSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VueSpringTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
